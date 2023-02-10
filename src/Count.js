64659b3cffdb1c03937072b182d91c49212341c8
= <=>
2023-02-10 11:36:05 +0530

    first commit

--- src/Count.js
+++ src/Count.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 
-var c=0;
 function useCounter(forward = true){
     const [counter, setCounter] = useState(0);
   
@@ -11,7 +10,6 @@ function useCounter(forward = true){
         }
         else{ setCounter(counter => counter-1) };
       }, 1000)
-      console.log(interval + 'and' + (c++));
 
       return () => clearInterval(interval);
     }, [forward])
